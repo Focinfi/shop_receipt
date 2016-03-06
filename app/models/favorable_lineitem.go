42e07aa7093d47f857cee3b48b92d305099cae80
Focinfi <focinfi@gmail.com>
2016-03-06 15:58:59 +0800

    Receipt: Add Message() for producing shopping list in plain text

--- app/models/favorable_lineitem.go
+++ app/models/favorable_lineitem.go
@@ -1,7 +1,19 @@
 package models
 
+import (
+	"fmt"
+)
+
 type FavorableLineItem struct {
 	ProductBarCode string
 	Quantity       int
 	Type           string
+}
+
+func (f FavorableLineItem) String() string {
+	product, ok := products[f.ProductBarCode]
+	if !ok {
+		panic(fmt.Sprintf("FavorableLineItem#String: has no product with BarCode %s", f.ProductBarCode))
+	}
+	return fmt.Sprintf("name: %s, quantity: %d", product.Name, f.Quantity)
 }
--- app/models/receipt.go
+++ app/models/receipt.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"github.com/Focinfi/shop_receipt/config"
 	"regexp"
 	"strconv"
 	"strings"
@@ -9,6 +10,29 @@ import (
 
 type Receipt struct {
 	LineItems map[string]*LineItem
+}
+
+func NewReceipt(barCodes []string) *Receipt {
+	lineItems := map[string]*LineItem{}
+	// init lineItems
+	for _, barCode := range barCodes {
+		quantityCode := regexp.MustCompile("-[0-9]+$").FindString(barCode)
+		productBarCode := barCode
+		quantity := 1
+		if quantityCode != "" {
+			productBarCode = strings.TrimSuffix(productBarCode, quantityCode)
+			quantityCode = strings.TrimPrefix(quantityCode, "-")
+			quantity, _ = strconv.Atoi(quantityCode)
+		}
+
+		if li, ok := lineItems[productBarCode]; !ok {
+			lineItems[productBarCode] = NewLineItem(productBarCode, quantity)
+		} else {
+			li.Quantity += quantity
+		}
+	}
+
+	return &Receipt{LineItems: lineItems}
 }
 
 func (r Receipt) Total() float64 {
@@ -45,25 +69,25 @@ func (r Receipt) SpecialOffers() map[string][]FavorableLineItem {
 	return favorableLineItemsMap
 }
 
-func NewReceipt(barCodes []string) *Receipt {
-	lineItems := map[string]*LineItem{}
-	// init lineItems
-	for _, barCode := range barCodes {
-		quantityCode := regexp.MustCompile("-[0-9]+$").FindString(barCode)
-		productBarCode := barCode
-		quantity := 1
-		if quantityCode != "" {
-			productBarCode = strings.TrimSuffix(productBarCode, quantityCode)
-			quantityCode = strings.TrimPrefix(quantityCode, "-")
-			quantity, _ = strconv.Atoi(quantityCode)
-		}
+func (r Receipt) Message() string {
+	header := fmt.Sprintf("******<%s> shopping list\n", config.AppName)
+	footer := "************************\n"
+	intervalLine := "------------------------\n"
+	var lineItems string
+	var favorableLineItems string
+	var total string
 
-		if li, ok := lineItems[productBarCode]; !ok {
-			lineItems[productBarCode] = NewLineItem(productBarCode, quantity)
-		} else {
-			li.Quantity += quantity
+	for _, li := range r.LineItems {
+		lineItems += fmt.Sprintf("%v\n", li)
+	}
+
+	for name, lineItems := range r.SpecialOffers() {
+		favorableLineItems += fmt.Sprintf("%s:\n", name)
+		for _, li := range lineItems {
+			favorableLineItems += fmt.Sprintf("%v\n", li)
 		}
 	}
 
-	return &Receipt{LineItems: lineItems}
+	total = fmt.Sprintf("total: %.2f \n", r.Total())
+	return fmt.Sprint(header, lineItems, intervalLine, favorableLineItems, intervalLine, total, footer)
 }
